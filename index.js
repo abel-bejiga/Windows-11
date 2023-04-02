b27c2667244b6cf866b0b642d02781948db8365b
Abel Bejiga <104611692+abel-bejiga@users.noreply.github.com>
2023-04-02 04:00:28 -0400

    added login page and password functionality, start menu completed and is interactive and more

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 // windows and main intro logo ..on loading
-// $("main").addClass("reviel-triple")
-// $("#win-img").addClass("reviel").addClass("loading")
-// setTimeout(() => {
-//     $("#win-img").addClass("hide");
-// }, 4000);
 
+    $(".intro").removeClass('hide')
+$("#win-img").addClass("hide");
 
-// windows intro audio .. on loading
+$('#continue-btn').click(continueClicked);
+function continueClicked(){
+    $(this).addClass('hide')
+$("main").removeClass('hide').addClass("reviel-triple")
+$("#win-img").removeClass('hide').addClass("reviel").addClass("loading")
+setTimeout(() => {
+    $("#win-img").addClass("hide");
+    
+}, 4000);
+introPlay();
+openLogIn();
+}
 
-// let audio = document.querySelector("#intro-sound");
-//     function introPlay() {
-//         $("#win-img").addClass("loading")
-//         audio.play();
+/////////////////////////////////////////////////
 
+// windows intro audio .. on loading /////////
 
-//         console.log("should be playing...")
-//     }
+let audio = document.querySelector("#intro-sound");
+    function introPlay() {
+        $("#win-img").addClass("loading")
+        audio.play();
+    }
 
-// introPlay();
+//////////////////////////////////////////////////
+                                // login page /////
 
-
+// pin submission /////////////
 $("#pin-btn").click(pinBtnClick);
 function pinBtnClick(){
     pinCheck()
-}
 
+}
+/////////////////////////////
 function correctPin(){
     $('#input-pin').attr('id', 'input-pin-correct');
 }
@@ -42,18 +53,26 @@ let pinCheck = () => {
     let pin = $("#input-pin").val();
     let show = $("#menu");
     let loginPage = $("#login");
-        // login pin result
+    let trialCount = 3;
+
+
+        // login pin result ///////////////////////////////
     pin = Number(pin);
         if (pin === 1234){
+            $('.pin-trials').addClass('hide')
             console.log("access granted!");
             correctPin();
             setTimeout(() => {
                 loginPage.addClass("hide");
-                $("h1").removeClass("hide");
+                // $("#sign-in-success h1").removeClass("hide");
+                openDeskTop();
             }, 1000);
 
             
-        } else if(isNaN(pin)) {
+        } else { 
+            trialCounter()
+            
+         if(isNaN(pin)) {
             console.log("pin should only be numbers")
             wrongPin();
         } else {
@@ -61,8 +80,10 @@ let pinCheck = () => {
             wrongPin();
 
         }
+    }
 
 }
+////////////////////////////////////////////
 
 // on enter press
 $("#input-pin").keypress(enterPressed)
@@ -71,4 +92,64 @@ function enterPressed(e){
     if (key == 13) {
        pinCheck();
     }
+    return key;
 }
+// login pin counter ////////////////////////////
+let countTrial = 4;
+
+        function trialCounter(){
+
+            if (countTrial > 1) {   
+             countTrial--;        
+             $('.pin-trials').removeClass('hide').html(`You have ${countTrial} trial left!`);
+             console.log(countTrial)   
+    
+        } else if (countTrial === 1){
+            $('#login').addClass('hide');
+            $('body').css('background', 'red')
+            $('.access-denied').removeClass('hide');
+
+        }
+}
+////////////////////////////////////////////
+        // opening needed html files //////////////////
+function openIntro(){
+            window.location.href = "./index.html"
+}
+function openDeskTop() {
+            window.location.href = "./desktop.html";        
+}
+const delay = ms => new Promise(res => setTimeout(res, ms));
+let openLogIn = async() => {
+       await delay(4000);
+        window.location.href = "./login.html"
+}
+///////////////////////////////////////
+
+                            // desktop
+
+// desktop searchbar onclick
+$("#taskbar-search").click(dsktopSearch)
+function dsktopSearch(){
+    $(this).css('text-indent', '33px');
+    
+}
+// $(".start-search input").click(startSearch)
+// function startSearch(){
+//     $(this).toggleCss('text-indent', '45px');
+    
+// }
+// adding date and time on desktop
+
+$("#current-time").html(timeDisplay());
+$("#current-date").html(dateDisplay());
+
+                // desktop funcitonality //////
+                // start menu ////
+$("#start-btn").click(startBtnClicked);
+    function startBtnClicked(){
+        $('#start-menu').toggleClass('slide-down slide-up').toggleClass('show no-display');
+
+    }
+
+
